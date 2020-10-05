7a244a4d4b6d2987e9cf757edbfcd50c241cdc61
Benjamin Wang <42283881+benwang2@users.noreply.github.com>
2020-10-04 21:49:49 -0400

    Update main.py
    Replaced template code with my own.

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,28 @@
 import os
-from discord.ext import commands
+import discord, asyncio
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+client = discord.Client()
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+__TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+resources = [
+    [["!shine upstrong", "!shine ustrong", "!shine upsmash", "!shine usmash"],"https://gfycat.com/kaleidoscopicbeneficialflicker"],
+    [["!shine upstrong gc","!shine ustrong gc","!shine upstrong gcc","!shine ustrong gcc"],"https://gfycat.com/meatydimpledamericancicada + *(z bound to strong)*"],
+    [["!pityflip","!pity flip"],"https://gfycat.com/whisperedeagerbuck"],
+    [["!pityflip gcc","!pity flip gcc"],"https://gfycat.com/grayhighlevelarrowworm"],
+    [["!shine","!shine angles"],"https://i.imgur.com/Zf6Ywes.png"],
+    [["!backshot"],"https://gfycat.com/IndolentShortIndigowingedparrot"],
+    [["!shmoost"],"https://gfycat.com/unknownforsakenaztecant-rivalsofaether"],
+    [["!betterburn"],"```\nCommands\n1. !shine upstrong\n2. !shine upstrong gcc\n3. !pity flip\n4. !pity flip gcc\n5. !shine angles```"]
+]
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+@client.event
+async def on_message(message):
+    if (message.author==client.user or message.author.bot):
+        return
+    for r in resources:
+        if message.content.lower() in r[0]:
+            await message.channel.send(r[1])
+            return
+
+client.run(__TOKEN)
