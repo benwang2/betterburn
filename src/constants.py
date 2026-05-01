43347f4d00d45991cdb7ff20c04d9956d58401aa
benwang2 <42283881+benwang2@users.noreply.github.com>
2026-05-01 18:34:44 -0400

    feat: add color constants

--- src/constants.py
+++ src/constants.py
@@ -14,6 +14,18 @@ class Rank(Enum):
     Aetherean = 8
 
 
+class Colors(Enum):
+    Stone = 0x000000
+    Bronze = 0x815502
+    Silver = 0xB4B4C3
+    Gold = 0xF1C40F
+    Platinum = 0xA9C3DA
+    Diamond = 0x0DDABE
+    Master = 0x2ECC71
+    Grandmaster = 0xE81042
+    Aetherean = 0xD508E2
+
+
 class RoleDoctorOption(Enum):
     check = 0
     test = 1
